204031ca5bea942ff597101ee6cb8894502cdc26
Алексей Прохин <alexsey.prohin@glonassmobile.com>
2022-02-04 23:42:48 +0300

    add repoDis

--- src/main/java/letscode/github/GitHubJob.java
+++ src/main/java/letscode/github/GitHubJob.java
@@ -1,14 +1,18 @@
 package letscode.github;
 
 import org.kohsuke.github.GHMyself;
+import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class GitHubJob {
     private final GitHub gitHub;
     private final Gui gui = new Gui();
-
+    private final Set <Long> allPrsId = new HashSet<>();
     public GitHubJob() {
         try {
             gitHub = new GitHubBuilder()
@@ -24,5 +28,46 @@ public class GitHubJob {
         GHMyself myself = gitHub.getMyself();
         String login = myself.getLogin();
 
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    myself.getAllRepositories()
+                            .values()
+                            .stream()
+                            .map(repository -> {
+                                try {
+                                    HashSet<GHPullRequest> newPrs = new HashSet<>();
+
+                                    List<GHPullRequest> prs = repository.queryPullRequests()
+                                            .list()
+                                            .toList();
+                                    Set<Long> prIds = prs.stream()
+                                            .map(GHPullRequest::getId)
+                                            .collect(Collectors.toSet());
+                                    prIds.removeAll(allPrsId);
+                                    allPrsId.addAll(prIds);
+                                    prs.forEach(pr -> {
+                                        if (prIds.contains(pr.getId())) {
+
+                                        }
+                                    });
+                                    return new RepositoryDescription(
+                                            repository.getFullName(),
+                                            repository,
+                                            prs
+                                    );
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }, 1000, 1000);
+
     }
+
 }
--- src/main/java/letscode/github/Gui.java
+++ src/main/java/letscode/github/Gui.java
@@ -3,12 +3,15 @@ package letscode.github;
 import java.awt.*;
 
 public class Gui {
+
+    private final TrayIcon trayIcon;
+
     public Gui() {
         try {
             SystemTray tray = SystemTray.getSystemTray();
             Image image = Toolkit.getDefaultToolkit()
                     .createImage(getClass().getResource("/logo.png"));
-            TrayIcon trayIcon = new TrayIcon(image, "GitHub helper");
+            trayIcon = new TrayIcon(image, "GitHub helper");
             trayIcon.setImageAutoSize(true);
             trayIcon.setToolTip("Github helper");
 
@@ -16,5 +19,9 @@ public class Gui {
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
+    }
+    public void showNotification(String title, String text) {
+        trayIcon.displayMessage(title, text, TrayIcon.MessageType.INFO);
+
     }
 }
